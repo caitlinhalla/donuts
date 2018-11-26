9b1abbdda7d65b035edbdc7b70b20970a5b77072
challa <challa@newrelic.com>
2018-11-25 18:55:28 -0800

    Read contents of specified file and write to new text file.

--- donuts.py
+++ donuts.py
@@ -3,7 +3,7 @@ import argparse
 def parse_args():
    parser = argparse.ArgumentParser()
    parser.add_argument('-p', '--filepath', help='Specify a file path', dest='filepath', required=True)
-   #ßparser.add_argument('-v', '--version', help='Specify the target upgrade version of .NET Core.', dest='version', required=True, choices=['2.0', '2.1'])
+   #parser.add_argument('-v', '--version', help='Specify the target upgrade version of .NET Core.', dest='version', required=True, choices=['2.0', '2.1'])
 
    return parser.parse_args()
 
@@ -12,5 +12,10 @@ def main():
    write_file(args.filepath)
 
 def write_file(filepath):
-   with open(filepath) as file:
-      contents = file.read()
+    with open(filepath) as file1:
+      with open('file2.txt', 'w') as file2:
+         contents = file1.read()
+         file2.write(contents)
+
+if __name__ == "__main__":
+    main()
