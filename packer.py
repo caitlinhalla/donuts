bcc9cf3e8bf68cac35f9b52b374252a0ed25e6c4
challa <challa@newrelic.com>
2019-06-03 19:53:43 -0700

    add arg for obfuscators with ObfuscatorConfiguration object

--- packer.py
+++ packer.py
@@ -66,41 +66,54 @@ obfuscators = {
 }
 
 
+class ObfuscatorConfiguration:
+    def __init__(self, args):
+        self.input_filepath = args.input_filepath
+        self.output_filepath = args.output_filepath
+        self.variable_name = args.variable_name
+        self.obfuscator_list = args.obfuscator_list
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-if', '--inputFilepath',
-                        help='Specify an input file path name', dest='inputFilepath', required=True)
+                        help='Specify an input file path name', dest='input_filepath', required=True)
     parser.add_argument('-of', '--outputFilepath',
-                        help='Specify an output file path name', dest='outputFilepath', required=True)
+                        help='Specify an output file path name', dest='output_filepath', required=True)
+    parser.add_argument('-var', '--variable_name',
+                        help='Name of the variable into which the packed script will be stored', dest='variable_name', default="x")
+
+    parser.add_argument('-obs', '--obfuscators',
+                        help='Obfuscators used to pack script', dest='obfuscator_list', nargs="+", required=True)
 
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    write_file(args.inputFilepath, args.outputFilepath)
+    configuration = ObfuscatorConfiguration(args)
+    write_file(configuration)
 
 
-obfuscator_list = ["base64", "zlib", "base64"]
-
-
-def write_file(inputFilepath, outputFilepath):
-    with open(inputFilepath, "rb") as file1:
-        with open(outputFilepath, 'w') as file2:
+def write_file(configuration):
+    with open(configuration.input_filepath, "rb") as file1:
+        with open(configuration.output_filepath, 'w') as file2:
             contents = file1.read()
 
-            for x in obfuscator_list:
-                contents = obfuscators[x].obfuscate(contents)
+            for x in set(configuration.obfuscator_list):
                 file2.write(obfuscators[x].generate_imports())
 
-            file2.write("var_1 = '''")
-            file2.write(contents.decode('ascii'))
-            file2.write("'''\n")
+            for x in configuration.obfuscator_list:
+                contents = obfuscators[x].obfuscate(contents)
 
-            for x in obfuscator_list:
-                file2.write(obfuscators[x].generate_deobfuscator("var_1"))
+            file2.write(
+                f"{configuration.variable_name}= '''{contents.decode('ascii')}'''\n")
 
-            file2.write("exec(var_1)")
+            for x in configuration.obfuscator_list:
+                file2.write(obfuscators[x].generate_deobfuscator(
+                    f"{configuration.variable_name}"))
+
+            file2.write(f"exec({configuration.variable_name})")
 
 
 if __name__ == "__main__":
